7d3c58836edd3ccf225261c85eae596af2f4648a
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-22 18:00:04 -0700

    added prompts

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,38 @@ USE tracker_db;
 CREATE TABLE employees (
   employee_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name INTEGER (10) NOT NULL,
+  last_name VARCHAR  (10) NOT NULL,
   manager VARCHAR (50) NOT NULL,
+  role_id INT,
   PRIMARY KEY (employee_id),
   FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
 
+
+INSERT INTO employees(first_name, last_name, manager)
+VALUES ( "Michael","Browning","Kim Yung"),
+				("Colleen","Finazzo","Kim Yung"),
+                ("Selena", "Mendez", "Kim Yung"),
+                ("John", "Dupree", "Kim Yung"),
+                ("Tim", "Morse", "Kim Yung"),
+                ("Abrielle", "Sanders", "Sandra Somerville"),
+				("Jeffrey", "Blane", "Sandra  Somerville"),
+			("Rebecca", "Morris", "Sandra Somerville"),
+			("Ronald", "Brewster", "Sandra Somerville"),
+			("Shawn", "Sanders", "Chris Guitierrez"),
+			("Tiffany", "Hatcher", "Chris Guitierrez"),
+			("Louis", "Reiner", "Chris Guitierrez"),
+			("Jayben", "Lee", "Chris Guitierrez"),	
+			("Staphanie", "Barboza", "Chris Guitierrez"),
+			("Alexis", "Hayword", "Richard Gilmore"),
+			("Dean", "Mitchell", "Richard Gilmore"),
+			("Ian", "Moore", "Richard Gilmore"),
+			("Jamal", "Peters", "Richard Gilmore"),
+			("Tahir", "Lang", "Emily Packer"),
+			("Chelsea", "Smith", "Emily Packer"),
+			("Samuel", "Singh", "Emily Packer"),
+			("Jesse", "Wong", "Emily Packer");
+		
 
 
 CREATE TABLE departments (
@@ -21,10 +47,44 @@ CREATE TABLE departments (
   PRIMARY KEY (department_id)
 );
 
+INSERT INTO departments (departments)	
+VALUES ("Sales"),
+			("Engineering"),
+			("Finance"),
+            ("Design"),
+            ("Legal");
+            
+SELECT * FROM departments;
+
 CREATE TABLE roles (
   role_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary INT (20),
-  PRIMARY KEY (role_id)
+  salary INT,
+  department_id INT,
+  PRIMARY KEY (role_id),
   FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
+
+INSERT INTO roles (title, salary)
+VALUES ("Sales Lead", 65000),
+				("Sales Rep", 55000),
+                ("Sales Administrator", 45000),
+                ("Sales Manager", 75000),
+                ("Design Lead", 65000),
+                ("Designer", 55000),
+                ("Design Administrator", 45000),
+                ("Design Manager", 75000),
+                ("Finance Lead", 65000),
+                ("Finance Associate", 55000),
+                ("Finance Administrator", 45000),
+                ("Legal Executive Lead", 85000),
+                ("Legal Rep", 75000),
+                ("Legal Admin", 55000),
+                ("Legal Executive Manager", 95000),
+                ("Engineer Team Lead", 70000),
+                ("Software Engineer", 65000),
+                ("Engineering Administrator", 55000),
+                ("Engineering Manager", 75000);
+                
+                
+SELECT * FROM employees;
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
 
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-
 const connection = mysql.createConnection({
   host: 'localhost',
 
@@ -18,3 +15,159 @@ const connection = mysql.createConnection({
   password: 'password',
   database: 'tracker_db',
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  runSearch();
+});
+
+const runSearch = () => {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View all employees',
+        'View all roles',
+        'View all departments',
+        'Add employee',
+        'Add department',
+        'Add role',
+        'Delete employee',
+        'Delete department',
+        'Delete role',
+        'Exit'
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View all employees':
+          viewEmployee();
+          break;
+
+        case 'View all roles':
+          viewRole();
+          break;
+
+        case 'View all departments':
+          viewDepartments();
+          break;
+
+        case 'Add employee':
+          addEmployee();
+          break;
+
+        case 'Add department':
+          addDepartment();
+          break;
+
+        case 'Add role':
+          addRole();
+          break;
+
+        case 'Delete employee':
+          deleteEmployee();
+          break;
+
+        case 'Delete department':
+          deleteDepartment();
+          break;
+
+        case 'Delete role':
+          deleteRole();
+          break;
+
+        case 'exit':
+          exit();
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
+
+//function to render all employees
+const viewEmployee = () => {
+  const query = 'SELECT * FROM employees';
+  connection.query(query, (err, res) => {
+    console.table(res);
+    runSearch();
+  })
+};
+//function to render all roles
+const viewRole = () => {
+  const query = 'SELECT * FROM roles';
+  connection.query(query, (err, res) => {
+    console.table(res);
+    runSearch();
+  })
+};
+//function to render all departments
+const viewDepartments = () => {
+  const query = 'SELECT * FROM departments';
+  connection.query(query, (err, res) => {
+    console.table(res);
+    runSearch();
+  })
+};
+
+const addEmployee = () => {
+  inquirer
+  .prompt ([
+    {
+      name: 'first_name',
+      type: 'input',
+      message: 'What is the employees first name?'
+    },
+    {
+      name: 'last_name',
+      type: 'input',
+      message: 'What is the employees last name?'
+    },
+    {
+      name: 'manager',
+      type: 'input',
+      message: 'Who is the employees manager?'
+    }
+  ])
+  .then((answer) => {
+    const query = 'INSERT INTO employees SET ?';
+    connection.query(query, 
+      {
+        first_name: answer.first_name,
+        last_name: answer.last_name,
+        manager: answer.manager
+      },
+        (err, res) => {
+        if (err) throw err;
+      console.log('employee added!');
+      runSearch();
+    })
+  })
+};
+//function to add department
+const addDepartment = () => {
+  inquirer
+  .prompt ([
+    {
+      name: 'department',
+      type: 'input',
+      message: 'What department you want to add?'
+    }
+  ])
+  .then((answer) => {
+    const query = 'INSERT INTO departments SET ?';
+    connection.query(query, 
+      {
+        departments: answer.department;
+      },
+        (err, res) => {
+        if (err) throw err;
+      console.log('department added!');
+      runSearch();
+    })
+  })
+};
